9babe7c079e696fd98d124e033b8f743f3a6b3af
iorio170894 <sasy.iorio@hotmail.it>
2020-09-14 15:04:50 +0200

    Finita la parte di registrazione a Cognito con anche il controllo per Password e Conferma Password. Definita activity con Probress bar Loading

--- app/src/main/java/com/example/tripnaples/ActivityRegistrazione.java
+++ app/src/main/java/com/example/tripnaples/ActivityRegistrazione.java
@@ -1,18 +1,23 @@
 package com.example.tripnaples;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
+
+import static android.widget.Toast.*;
 
 public class ActivityRegistrazione extends AppCompatActivity {
 
@@ -20,9 +25,8 @@ public class ActivityRegistrazione extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione);
-        registerUser();
 
-        //Tasto annulla torna a MainActivity
+        registerUser();
 
         Button annulla = (Button) findViewById(R.id.buttonAnnulla);
         annulla.setOnClickListener(new View.OnClickListener() {
@@ -37,9 +41,9 @@ public class ActivityRegistrazione extends AppCompatActivity {
         private void registerUser() {
             final EditText inputEmail = findViewById(R.id.editTextMail);
             final EditText inputPassword = findViewById(R.id.editTextTextPassword);
+            final EditText inputConfermaPassword = findViewById(R.id.editTextTextConfirmPassword);
             final EditText inputNickname = findViewById(R.id.editTextNickname);
-            //Log.i("Tag","Email:" + inputEmail);
-            //Log.i("Tag","Password:" + inputPassword);
+
 
             //Crea un CognitoUserAttributes e un user Attributes
 
@@ -60,27 +64,53 @@ public class ActivityRegistrazione extends AppCompatActivity {
                         //user è stato confermato
                         Log.i("Tag", "Registrazione avvenuta con successo... confermata");
                     }
+                  //  if (inputPassword.equals(inputConfermaPassword)) {
+                        loagindDialog.startLoadingDialog();
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                loagindDialog.dismissDialog();
+                                startActivity(new Intent(ActivityRegistrazione.this, ActivityBenvenuto.class));
+                            }
+                        }, 5000);
+                  //  }
                 }
 
                 @Override
                 public void onFailure(Exception exception) {
                     //registrazione fallita controlla l'eccezione
                     Log.i( "Tag","Registrazione fallita" + exception.getLocalizedMessage());
+                    AlertDialog.Builder builder=new AlertDialog.Builder(ActivityRegistrazione.this);
+                    builder.setTitle("Errore nella Registrazione:");
+                    builder.setMessage("Attenzione:"+exception.getLocalizedMessage());
+                    builder.show();
                 }
             };
+
             //Tasto avanti va avanti nella registrazione
 
             Button avanti = (Button) findViewById(R.id.buttonAvanti);
             avanti.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    //apri activity relativo alla seconda registrazione
-                    CognitoSettings cognitoSettings=new CognitoSettings(ActivityRegistrazione.this);
-                    cognitoSettings.getUserPool().signUpInBackground(String.valueOf(inputNickname.getText() )
-                        , String.valueOf(inputPassword.getText()),userAttributes,null,signupCallback
-                        );
-                    startActivity(new Intent(ActivityRegistrazione.this, ActivityRegistrazione2.class));
+
+                   if (!inputPassword.getText().toString().equals(inputConfermaPassword.getText().toString())) {
+                       AlertDialog.Builder builder=new AlertDialog.Builder(ActivityRegistrazione.this);
+                       builder.setTitle("Errore nella Registrazione:");
+                       builder.setMessage("Attenzione: Password e Conferma Password non coincidono");
+                       builder.show();
+                   }
+                   else {
+                       CognitoSettings cognitoSettings = new CognitoSettings(ActivityRegistrazione.this);
+                       cognitoSettings.getUserPool().signUpInBackground(String.valueOf(inputNickname.getText())
+                               , String.valueOf(inputPassword.getText()), userAttributes, null, signupCallback
+                       );
+                        }
                 }
             });
         }
+
+        LoagindDialog loagindDialog = new LoagindDialog(ActivityRegistrazione.this);
+
 }
